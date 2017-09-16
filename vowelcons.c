89a3a03b605bda8ecd3767f3bcc943bc46cbe7b8
GOKILADHARANISHYAMALA <31985290+GOKILADHARANISHYAMALA@users.noreply.github.com>
2017-09-16 08:17:57 +0530

    Create vowelcons.c

---
+++ vowelcons.c
@@ -0,0 +1,13 @@
+# -CODESIMPLE
+#include<stdio.h>
+#include<conio.h>
+char letter;
+scanf("%c",&letter);
+if(letter=='a'||letter=='e'||letter=='i'||letter=='o'||letter=='u'||letter=='A'||letter=='E'||letter=='I'||letter=='O'||letter=='U')
+{
+printf("\nvowel");
+}
+else
+{
+printf("\nconsonant);
+}
