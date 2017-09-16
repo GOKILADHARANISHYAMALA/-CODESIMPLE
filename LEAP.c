9f3831f428adbde95dbbe0295c65aafb52f0506b
GOKILA DHARANI SHYAMALA <31985290+GOKILADHARANISHYAMALA@users.noreply.github.com>
2017-09-16 16:24:27 +0530

    Create LEAP.c

---
+++ LEAP.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int year;
+scanf("%d",&year);
+if(year%4==0||year%400==0)
+{
+printf("\nleap year");
+}
+else
+{
+printf("\nnon-leap year");
+}
+}
